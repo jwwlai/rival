2788663c6a2b98c956f30ffe5aac57a9c29f9e97
Julia Lai <jwwlai@gmail.com>
2017-11-25 14:11:58 -0800

    working program

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+#!/usr/bin/env node
+
 var program = require("commander");
 var fs = require("file-system");
+
+// constants
+const DEPENDENCIES = "dependencies";
 
 // Initialize our "db" - really just a directory of files
 const databasePath = "db";
@@ -8,55 +13,173 @@ fs.mkdir(databasePath);
 program
 	.command("DEPEND <pack> [packs...]")
 	.action(function(pack, packs) {
-		// create a file for the package
-		// append the dependencies in the file
 		const path = `${databasePath}/${pack}`;
-		if (!fs.existsSync(path)) {
-			if (packs) {
-				var otherPacks = "Dependecies: ";
-				packs.forEach(function(p) {
-					otherPacks = otherPacks.concat(p + " ");
-				});
-				otherPacks = otherPacks.concat("\nRequired by: " + pack);
-				otherPacks = otherPacks.concat("\nInstalled: ");
-				fs.writeFile(path, otherPacks);
-			} else {
-				fs.writeFile(path, packs[0]);
-			}
+
+		// Add to dependencies list
+		if (fs.existsSync(path)) {
+			const dependencies = getFileField(pack, DEPENDENCIES);
+			packs.forEach((item) => {
+				if (!dependencies.includes(item)) {
+					dependencies.push(item);
+				}
+			});
+			writeToField(pack, DEPENDENCIES, dependencies);
 		} else {
-			console.log(`The package ${pack} already exists in the dependencies`);
+			var packJSON = {
+				dependencies: packs,
+				requiredBy: [],
+				installed: { explicit: false, implicit: false }
+			};
+			fs.writeFileSync(path, JSON.stringify(packJSON));
 		}
+
+		// Add to requiredBy list
+		packs.forEach((item) => {
+			const dependencyPath = `${databasePath}/${item}`;
+			if (fs.existsSync(dependencyPath)) {
+				let requiredBy = getFileField(item, "requiredBy");
+				const isAlreadyRequired = requiredBy.filter((item) => item === pack);
+				if (isAlreadyRequired.length === 0) {
+					requiredBy.push(pack);
+				}
+				writeToField(item, "requiredBy", requiredBy);
+			} else {
+				var packJSON = {
+					dependencies: [],
+					requiredBy: [pack],
+					installed:  { explicit: false, implicit: false }
+				};
+				fs.writeFileSync(dependencyPath, JSON.stringify(packJSON));
+			}
+		});
 	});
 
 program
 	.command("INSTALL <pack>")
-	.description(
-		"Takes 1 argument. Implicitly install any unmet dependencies, and then explicitly install <package>." +
-		"If <package> is already installed, echo that feedback."
-	)
 	.action(function(pack) {
-
-		// append the install line in the file if it exists, if not then create a new file for the package
 		const path = `${databasePath}/${pack}`;
 		if (fs.existsSync(path)) {
-			const fileContents = fs.readFileSync(path, "utf8");
+			const fileContents = JSON.parse(fs.readFileSync(path));
 
-			// Check the install file contents for the package by looking at the install row
-			var newFileContents = fileContents;
-			var installedByRow = newFileContents.split("\n").pop();
-			var installedPackages = installedByRow.split(" ").slice(1);
+			// Implicitly install dependencies recursively
+			const dependencies = fileContents[DEPENDENCIES];
+			install(dependencies);
 
-			if (installedPackages && !installedPackages.includes(pack)) {
-				console.log(`Installing ${pack}`);
-				installedPackages.push(pack);
+			// Explicitly install package
+			if (isPackageInstalled(pack)) {
+				console.log(`Package ${pack} is already installed.`);
 			} else {
-				console.log(`The package ${pack} is already installed`);
+				console.log(`Installing ${pack}...`);
+				writeToField(pack, "installed", { explicit: true, implicit: false });
 			}
-
-			fs.appendFileSync(path, installedByRow);
 		} else {
-			fs.writeFile(path, "");
+			console.log(`Installing ${pack}...`);
+			var initPackJSON = {
+				dependencies: [],
+				requiredBy: [],
+				installed: { explicit: true, implicit: false }
+			};
+			fs.writeFileSync(path, JSON.stringify(initPackJSON));
 		}
 	});
 
+program
+	.command("REMOVE <pack>")
+	.action(function(pack) {
+		const path = `${databasePath}/${pack}`;
+		if (fs.existsSync(path)) {
+			remove(pack);
+		} else {
+			console.log(`${pack} does not exist`);
+		}
+	});
+
+program
+	.command("LIST")
+	.action(function() {
+		fs.readdirSync(databasePath).filter((file) => {
+			if (isPackageInstalled(file)) {
+				console.log(file);
+			}
+		});
+	});
+
 program.parse(process.argv);
+
+// Helpers
+
+function install(dependencies) {
+	if (dependencies && dependencies.length > 0) {
+		dependencies.forEach((dep) => {
+			const depsList = getFileField(dep, DEPENDENCIES);
+
+			// mark installed
+			if (!isPackageInstalled(dep)) {
+				console.log(`Installing ${dep}...`);
+				writeToField(dep, "installed", { explicit: false, implicit: true });
+			}
+
+			// recurse with new list of deps
+			install(depsList);
+		});
+	}
+	return;
+}
+
+function remove(pack) {
+	if (isPackageInstalled(pack)) {
+		// 1. check if it's required by anything and see if any of them are installed
+		const packagesRequired = getFileField(pack, "requiredBy");
+		const isRequired = packagesRequired.length > 0;
+		if (isRequired && isAnyInstalled(packagesRequired)) {
+			console.log(`Package ${pack} is still needed - cannot remove.`);
+		} else {
+			// If none of the required by packages are installed, uninstall package and start over with its dependencies.
+			console.log(`Removing ${pack}...`);
+			writeToField(pack, "installed", { explicit: false, implicit: false });
+
+			// Only remove implicitly installed packages
+			getFileField(pack, DEPENDENCIES)
+				.filter((dep) => {
+					return isImplicitlyInstalled(dep) === true;
+				})
+				.forEach((dep) => {
+					remove(dep);
+				});
+		}
+	} else {
+		console.log(`${pack} is not installed.`);
+	}
+}
+
+function isPackageInstalled(pack) {
+	return isExplicitlyInstalled(pack) || isImplicitlyInstalled(pack);
+}
+
+function isExplicitlyInstalled(pack) {
+	return getFileField(pack, "installed")["explicit"];
+}
+
+function isImplicitlyInstalled(pack) {
+	return getFileField(pack, "installed")["implicit"];
+}
+
+function isAnyInstalled(list) {
+	return list.some((item) => { return isPackageInstalled(item) });
+}
+
+function getFileField(pack, field) {
+	const fileContents = JSON.parse(fs.readFileSync(`${databasePath}/${pack}`));
+	return fileContents[field];
+}
+
+function getFileContents(pack) {
+	return JSON.parse(fs.readFileSync(`${databasePath}/${pack}`));
+}
+
+function writeToField(pack, field, value) {
+	const path = `${databasePath}/${pack}`;
+	const contents = getFileContents(pack);
+	contents[field] = value;
+	fs.writeFileSync(path, JSON.stringify(contents));
+}
